6839db73b6b8bd9446fdb8ec7a52cb8ba4972479
Audrey Xie <audrey.hy.xie@gmail.com>
2019-11-06 12:21:44 -0800

    Tank code and make project compatible with updated WPILib extension

--- src/main/java/frc/modes/DriverControl.java
+++ src/main/java/frc/modes/DriverControl.java
@@ -17,67 +17,19 @@ import frc.robot.Robot;
  */
 class DriverControl extends Mode {
 
-    private int pov = -1;
-    private int lastPov;
-
     @Override
     public boolean loop() {
-        driveSwerve();
+        driveTank();
         return true;
     }
 
-    private void driveSwerve() {
-        if (Input.SWERVE_XBOX.getStartButtonPressed())
-            Robot.SWERVE.setRobotCentric(true);
-        if (Input.SWERVE_XBOX.getBackButtonPressed())
-            Robot.SWERVE.setRobotCentric(false);
+    private void driveTank() {
 
-        double x = JoystickProfile.applyDeadband(Input.SWERVE_XBOX.getY(Hand.kLeft));
-        double y = JoystickProfile.applyDeadband(-Input.SWERVE_XBOX.getX(Hand.kLeft));
-        // double y =
-        // JoystickProfile.applyDeadband(Input.SWERVE_XBOX.getX(Hand.kRight));
-        double mag = Math.sqrt(x * x + y * y);
-        x *= mag;
-        y *= mag;
-        boolean buttonPressed = false;
-        if (pov == -1) {
-            buttonPressed = true;
-        }
-        pov = Input.SWERVE_XBOX.getPOV();
-        if (Input.SWERVE_XBOX.getBumperPressed(Hand.kLeft)) {
-            pov = lastPov - 45;
-        }
-        if (Input.SWERVE_XBOX.getBumperPressed(Hand.kRight)) {
-            pov = lastPov + 45;
-        }
-        if (buttonPressed) {
-            if (pov == -1) {
-            } else if (pov == 45) {
-                Robot.SWERVE.setAngle(Math.toRadians(-60));
-                lastPov = pov;
-            } else if (pov == 135) {
-                Robot.SWERVE.setAngle(Math.toRadians(-150));
-                lastPov = pov;
-            } else if (pov == 235) {
-                Robot.SWERVE.setAngle(Math.toRadians(-300));
-                lastPov = pov;
-            } else if (pov == 325) {
-                Robot.SWERVE.setAngle(Math.toRadians(-210));
-                lastPov = pov;
-            } else {
-                Robot.SWERVE.setAngle(Math.toRadians(-pov));
-                lastPov = pov;
-            }
+        // You may be wondering, why x is .getY() and y is .getX()? Don't question it //
+        double x = JoystickProfile.applyDeadband(-Input.TANK_XBOX.getY(Hand.kLeft));
+        double y = JoystickProfile.applyDeadband(Input.TANK_XBOX.getX(Hand.kLeft));
 
-        }
-
-        double lTrigger = Input.SWERVE_XBOX.getTriggerAxis(Hand.kLeft);
-        double rTrigger = Input.SWERVE_XBOX.getTriggerAxis(Hand.kRight);
-        double rotate = 0;
-        if (lTrigger + rTrigger > 0.05) {
-            rotate = -(rTrigger * rTrigger - lTrigger * lTrigger);
-        }
-        Robot.SWERVE.drive(x, y, rotate);
+        Robot.TANK.setPolar(x, y);
     }
 
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,16 +7,12 @@
 
 package frc.robot;
 
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.TimedRobot;
 import frc.config.Config;
-import frc.input.Input;
-import frc.input.JoystickProfile;
 import frc.modes.Mode;
-import frc.swerve.NavXGyro;
-import frc.swerve.Swerve;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import frc.tank.Tank;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -26,72 +22,91 @@ import frc.swerve.Swerve;
  * project.
  */
 public class Robot extends TimedRobot {
+  private NetworkTableEntry mode;
+  private Autonomous autonomous;
 
-    private NetworkTableEntry mode;
-    private Autonomous autonomous;
+  public static Tank TANK;
+  public static double ROBOT_WIDTH;
+  public static double ROBOT_HEIGHT;
+  public static double ROBOT_RADIUS;
 
-    public static Swerve SWERVE;
-    public static NavXGyro GYRO;
-    public static double ROBOT_WIDTH;
-    public static double ROBOT_HEIGHT;
-    public static double ROBOT_RADIUS;
+  /**
+   * This function is run when the robot is first started up and should be
+   * used for any initialization code.
+   */
+  @Override
+  public void robotInit() {
+    Config.start();
+    ROBOT_WIDTH = Config.getDouble("robot_width");
+    ROBOT_HEIGHT = Config.getDouble("robot_height");
+    ROBOT_RADIUS = Math.sqrt(ROBOT_WIDTH * ROBOT_WIDTH + ROBOT_HEIGHT * ROBOT_HEIGHT) / 2;
+    autonomous = new Autonomous(this);
+    TANK = new Tank();
+    Mode.initModes();
+    mode = NetworkTableInstance.getDefault().getTable("Robot").getEntry("mode");
+    mode.setNumber(0);
+  }
 
-    private boolean overridden;
-
-    @Override
-    public void robotInit() {
-        Config.start();
-        ROBOT_WIDTH = Config.getDouble("robot_width");
-        ROBOT_HEIGHT = Config.getDouble("robot_height");
-        ROBOT_RADIUS = Math.sqrt(ROBOT_WIDTH * ROBOT_WIDTH + ROBOT_HEIGHT * ROBOT_HEIGHT) / 2;
-        autonomous = new Autonomous(this);
-        GYRO = new NavXGyro();
-        SWERVE = new Swerve();
-        Mode.initModes();
-        mode = NetworkTableInstance.getDefault().getTable("Robot").getEntry("mode");
+  private void loop() {
+    autonomous.loop();
+    int i = mode.getNumber(0).intValue();
+    if (!Mode.getMode(i).loop()) {
+        autonomous.modeFinished();
         mode.setNumber(0);
     }
+  }
 
-    private void loop() {
-        // long start = System.nanoTime();
-        // handle mode switching
-        autonomous.loop();
-        int i = mode.getNumber(0).intValue();
-        if (manualOverride()) {
-            autonomous.kill();
-            mode.setNumber(0);
-            i = 0;
-        }
-        if (!Mode.getMode(i).loop()) {
-            autonomous.modeFinished();
-            mode.setNumber(0);
-        }
-    }
+  public void setMode(int i) {
+      mode.setNumber(i);
+  }
 
-    public void setMode(int i) {
-        mode.setNumber(i);
-    }
+  /**
+   * This function is called every robot packet, no matter the mode. Use
+   * this for items like diagnostics that you want ran during disabled,
+   * autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before
+   * LiveWindow and SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+  }
 
-    private boolean manualOverride() {
-        double x = JoystickProfile.applyDeadband(-Input.SWERVE_XBOX.getY(Hand.kLeft));
-        double y = JoystickProfile.applyDeadband(Input.SWERVE_XBOX.getX(Hand.kLeft));
-        boolean temp = !(x == 0 && y == 0);
-        if (temp && !overridden) {
-            overridden = temp;
-            return true;
-        }
-        overridden = temp;
-        return false;
-    }
+  /**
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different autonomous modes using the dashboard. The sendable
+   * chooser code works with the Java SmartDashboard. If you prefer the
+   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+   * getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to
+   * the switch structure below with additional strings. If using the
+   * SendableChooser make sure to add them to the chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+  }
 
-    @Override
-    public void autonomousPeriodic() {
-        loop();
-    }
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+    loop();
+  }
 
-    @Override
-    public void teleopPeriodic() {
-        loop();
-    }
+  /**
+   * This function is called periodically during operator control.
+   */
+  @Override
+  public void teleopPeriodic() {
+    loop();
+  }
 
+  /**
+   * This function is called periodically during test mode.
+   */
+  @Override
+  public void testPeriodic() {
+  }
 }
