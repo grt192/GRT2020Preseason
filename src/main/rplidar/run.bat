bb92ad835e7669b7496cad06128209d7423cf591
Michael Carlstromls <rmc@carlstrom.com>
2019-11-08 15:10:27 -0800

    Java J no longer required in java 11

--- src/main/rplidar/run.bat
+++ src/main/rplidar/run.bat
@@ -1,9 +1,6 @@
 javac RPLidar.java
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-javah RPLidarDriver
-if %errorlevel% neq 0 exit /b %errorlevel%
-
 C:\Users\Public\frc2019\roborio\bin\arm-frc2019-linux-gnueabi-g++.exe -IC:\Users\Public\frc2019\jdk\include -IC:\Users\rmc.CARLSTROM\Documents\rplidar_sdk\sdk\sdk\include -IC:\Users\rmc.CARLSTROM\Documents\rplidar_sdk\sdk\sdk\src -I. -fPIC rplidar.cpp librplidar_sdk.a -lpthread -shared -o librplidar.so
 if %errorlevel% neq 0 exit /b %errorlevel%
 
